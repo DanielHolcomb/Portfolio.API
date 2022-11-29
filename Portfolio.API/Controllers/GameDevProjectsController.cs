32cc69ca7e3f0316ba0fc182a0d02ef7b5df7ddd
Dan <me@danielholcomb.com>
2022-11-28 18:14:00 -0700

    Add 24 expiry caching for data

--- Portfolio.API/Controllers/GameDevProjectsController.cs
+++ Portfolio.API/Controllers/GameDevProjectsController.cs
@@ -2,6 +2,8 @@ using Microsoft.AspNetCore.Mvc;
 using Core.HttpDynamo;
 using Core.JwtBuilder;
 using System.Security.Claims;
+using Microsoft.Extensions.Caching.Memory;
+using Portfolio.API.Models;
 
 namespace Portfolio.API.Controllers
 {
@@ -13,30 +15,54 @@ namespace Portfolio.API.Controllers
         private readonly ILogger<GameDevProjectsController> _logger;
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly IConfiguration _config;
+        private readonly IMemoryCache _memoryCache;
 
-        public GameDevProjectsController(ILogger<GameDevProjectsController> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        private const string LudumCacheDareKey = "LudumDareGames";
+        private const string SteamCacheKey = "SteamGames";
+
+        public GameDevProjectsController(ILogger<GameDevProjectsController> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration, IMemoryCache memoryCache)
         {
             _logger = logger;
             _httpClientFactory = httpClientFactory;
             _config = configuration;
+            _memoryCache = memoryCache;
         }
 
         [HttpGet]
         [Route("LudumDare")]
         public async Task<IActionResult> GetLudumDareProjects()
         {
-            var token = JwtTokenGenerator.GenerateToken(_config, new[] { new Claim("source", "portfolio") });
-            var response = await HttpDynamo.GetRequestAsync(_httpClientFactory, "https://projectsludumdare20221120164815.azurewebsites.net/Projects/LudumDare/Games/dandala88", token);
-            return Ok(response);
+            LudumDareData cachedResponse;
+            if (_memoryCache.TryGetValue(LudumCacheDareKey, out cachedResponse))
+            {
+                return Ok(cachedResponse);
+            }
+            else
+            {
+                var token = JwtTokenGenerator.GenerateToken(_config, new[] { new Claim("source", "portfolio") });
+                var response = await HttpDynamo.GetRequestAsync< LudumDareData>(_httpClientFactory, "https://projectsludumdare20221120164815.azurewebsites.net/Projects/LudumDare/Games/dandala88", token);
+                _memoryCache.Set(LudumCacheDareKey, response, DateTime.UtcNow + TimeSpan.FromHours(24));
+                return Ok(response);
+            }
         }
 
         [HttpGet]
         [Route("Steam")]
         public async Task<IActionResult> GetSteamProjects()
         {
-            var token = JwtTokenGenerator.GenerateToken(_config, new[] { new Claim("source", "portfolio") });
-            var response = await HttpDynamo.GetRequestAsync(_httpClientFactory, "https://projectssteam20221120215756.azurewebsites.net/Steam/Game?appId=1648160&appid=1940550", token);
-            return Ok(response);
+            List<SteamData> cachedResponse;
+            if (_memoryCache.TryGetValue(SteamCacheKey, out cachedResponse))
+            {
+                return Ok(cachedResponse);
+            }
+            else
+            {
+                var token = JwtTokenGenerator.GenerateToken(_config, new[] { new Claim("source", "portfolio") });
+                var response = await HttpDynamo.GetRequestAsync<List<SteamData>>(_httpClientFactory, "https://projectssteam20221120215756.azurewebsites.net/Steam/Game?appId=1648160&appid=1940550", token);
+                _memoryCache.Set(SteamCacheKey, response, DateTime.UtcNow + TimeSpan.FromHours(24));
+
+                return Ok(response);
+            }
         }
     }
 }
--- Portfolio.API/Controllers/WebDevProjectsController.cs
+++ Portfolio.API/Controllers/WebDevProjectsController.cs
@@ -1,7 +1,9 @@
 ﻿using Core.HttpDynamo;
 using Core.JwtBuilder;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
 using Portfolio.API.Models;
+using System;
 using System.Security.Claims;
 
 namespace Portfolio.API.Controllers
@@ -13,41 +15,57 @@ namespace Portfolio.API.Controllers
         private readonly ILogger<GameDevProjectsController> _logger;
         private readonly IHttpClientFactory _httpClientFactory;
         private readonly IConfiguration _config;
+        private readonly IMemoryCache _memoryCache;
 
-        public WebDevProjectsController(ILogger<GameDevProjectsController> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration)
+        private const string GitHubRepoListCachedKey = "GetHubRepoList";
+
+        public WebDevProjectsController(ILogger<GameDevProjectsController> logger, IHttpClientFactory httpClientFactory, IConfiguration configuration, IMemoryCache memoryCache)
         {
             _logger = logger;
             _httpClientFactory = httpClientFactory;
             _config = configuration;
+            _memoryCache = memoryCache;
         }
 
         [HttpGet]
         [Route("DotNet")]
         public async Task<IActionResult> GetDotNetProjects()
         {
-            var token = JwtTokenGenerator.GenerateToken(_config, new[] { new Claim("source", "portfolio") });
-            var resourceGroupList = await HttpDynamo.GetRequestAsync<List<ResourceGroup>>(_httpClientFactory, "https://projectsazure20221128090301.azurewebsites.net/Azure/Resource/Groups", token);
-
-            var resources = new List<Resource>();
-
-            foreach(var resourceGroup in resourceGroupList)
-            {
-                var resourceList = await HttpDynamo.GetRequestAsync<List<Resource>>(_httpClientFactory, $"https://projectsazure20221128090301.azurewebsites.net/Azure/Resources/Sites/{resourceGroup.Name}", token);
-
-                if(resourceList != null)
-                    resources.AddRange(resourceList);
-            }
-
             var gitHubRepoList = new List<GitHubRepo>();
-            foreach(var resource in resources)
+
+            if (_memoryCache.TryGetValue(GitHubRepoListCachedKey, out gitHubRepoList))
             {
-                var repos = await HttpDynamo.GetRequestAsync<GitHubRepo>(_httpClientFactory, $"https://projectsgithub20221128090558.azurewebsites.net/repo{resource.RepoPath}", token);
-
-                if(repos != null)
-                    gitHubRepoList.Add(repos);
+                return Ok(gitHubRepoList);
             }
+            else
+            {
 
-            return Ok(gitHubRepoList);
+                var token = JwtTokenGenerator.GenerateToken(_config, new[] { new Claim("source", "portfolio") });
+                var resourceGroupList = await HttpDynamo.GetRequestAsync<List<ResourceGroup>>(_httpClientFactory, "https://projectsazure20221128090301.azurewebsites.net/Azure/Resource/Groups", token);
+
+                var resources = new List<Resource>();
+
+                foreach (var resourceGroup in resourceGroupList)
+                {
+                    var resourceList = await HttpDynamo.GetRequestAsync<List<Resource>>(_httpClientFactory, $"https://projectsazure20221128090301.azurewebsites.net/Azure/Resources/Sites/{resourceGroup.Name}", token);
+
+                    if (resourceList != null)
+                        resources.AddRange(resourceList);
+                }
+
+                gitHubRepoList = new List<GitHubRepo>();
+                foreach (var resource in resources)
+                {
+                    var repos = await HttpDynamo.GetRequestAsync<GitHubRepo>(_httpClientFactory, $"https://projectsgithub20221128090558.azurewebsites.net/repo{resource.RepoPath}", token);
+
+                    if (repos != null)
+                        gitHubRepoList.Add(repos);
+                }
+
+                _memoryCache.Set(GitHubRepoListCachedKey, gitHubRepoList, DateTime.UtcNow + TimeSpan.FromHours(24));
+
+                return Ok(gitHubRepoList);
+            }
         }
     }
 }
